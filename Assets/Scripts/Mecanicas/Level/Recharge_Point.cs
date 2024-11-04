33792e632d0cebd2d4d78c47e36f6921accdafc0
Thanimator <theocavalcanti03@gmail.com>
2024-11-04 08:27:05 -0300

    Teste recurso

---
+++ Assets/Scripts/Mecanicas/Level/Recharge_Point.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//mover enum para class Ferramenta
+public enum ToolType {
+    Mangueira, ExtAgua, ExtQuimico
+}
+public class Recharge_Point : MonoBehaviour
+{
+    
+    //pegar  mecanicas de Key 
+    //Precisa criar uma classe Ferramenta para carregar seus ataques
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
