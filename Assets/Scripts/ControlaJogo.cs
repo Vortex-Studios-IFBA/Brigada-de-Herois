3b44bc30fba2966c5c9cc2f7c20df258e9f59030
Akashic002 <anselmojuniorsempre@gmail.com>
2025-01-14 19:11:55 -0300

    Ajuste e finalização do bbotão de rotação

--- Assets/Scripts/ControlaJogo.cs
+++ Assets/Scripts/ControlaJogo.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ControlaJogo : MonoBehaviour
-
-{  
+{
     public static ControlaJogo Instance { get; private set; }
 
     private bool pausado = false;
-
     public int missao;
-
-    public bool jogoVertical ;
+    public bool jogoVertical;
 
     public Text verticalTexto;
+
+    public delegate void CenaCarregadaHandler();
+    public static event CenaCarregadaHandler OnCenaCarregada;
+
+    
 
     private void Awake()
     {
@@ -29,51 +32,49 @@ public class ControlaJogo : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     private void Start()
     {
         AplicarRotacaoTela();
-
     }
 
-    // Update is called once per frame
-    public void CarregarCena(int cenaID)
-    {
-        SceneManager.LoadScene(cenaID);
-    }
     public void EntrarFase(int index)
     {
         missao = index;
         SceneManager.LoadScene(3);
+        SceneManager.sceneLoaded += CenaCarregada;
+    }
+    public void CarregarCena(int cenaID)
+    {
+        SceneManager.LoadScene(cenaID);
+        SceneManager.sceneLoaded += CenaCarregada;
+    }
+
+    private void CenaCarregada(Scene cena, LoadSceneMode modo)
+    {
+        SceneManager.sceneLoaded -= CenaCarregada;
+        OnCenaCarregada?.Invoke();
     }
 
     public void AplicarRotacaoTela()
     {
         if (jogoVertical)
         {
-            Screen.orientation = ScreenOrientation.AutoRotation; 
-            Screen.autorotateToPortrait = true;                  
-            Screen.autorotateToPortraitUpsideDown = true;        
-            Screen.autorotateToLandscapeLeft = false;            
+            Screen.orientation = ScreenOrientation.AutoRotation;
+            Screen.autorotateToPortrait = true;
+            Screen.autorotateToPortraitUpsideDown = true;
+            Screen.autorotateToLandscapeLeft = false;
             Screen.autorotateToLandscapeRight = false;
-
-            verticalTexto.text = jogoVertical.ToString();
         }
         else
         {
             Screen.orientation = ScreenOrientation.LandscapeLeft;
-            try
-            {
-                verticalTexto.text = jogoVertical.ToString();
-            }
-            
-            catch
-            {
-                Debug.Log("Trocou a rotacao: " + jogoVertical);
-            }
+
         }
+
+       
     }
 
-    //Associar no bot�o para trocar a rota��o do CELULAR
     public void TrocarRotacao()
     {
         jogoVertical = !jogoVertical;
@@ -81,3 +82,4 @@ public class ControlaJogo : MonoBehaviour
         Debug.Log("Trocou a rotacao: " + jogoVertical);
     }
 }
+
